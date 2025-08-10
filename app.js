611f4e8b01743202e6863dec4ea97a163a103ff9
Heshani833 <heshanishehana123@gmail.com>
2025-08-10 13:42:32 +0530

    connected mongodb

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+//atz0zLPX66IpqoaE
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//middleware
+app.use("/", (req, res, next) => {
+    res.send("HI");
+})
+
+mongoose.connect("mongodb+srv://admin:atz0zLPX66IpqoaE@cluster0.b7xdaxc.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(()=>{
+    app.listen(5000);
+})
+
+.catch((err) => console.log(err));
