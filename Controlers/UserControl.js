2defc33906c677128e1dcc55d528e2a98b69375f
Heshani833 <heshanishehana123@gmail.com>
2025-08-10 23:22:20 +0530

    data insert part of UserControl.js

--- Controlers/UserControl.js
+++ Controlers/UserControl.js
@@ -1,7 +1,7 @@
 const User = require("../Model/UserModel");
 
 const getAllUsers = async (req, res, next) => {
-    let User;
+    let users;
     try {
         users = await User.find();
     } catch (err) {
@@ -18,4 +18,31 @@ const getAllUsers = async (req, res, next) => {
     return res.status(200).json({ users });
 };
 
+//data insert
+const addUser = async(req, res, next) => {
+     const { name, email, age, address} = req.body;
+
+     let users;
+
+     try {
+        users = new users({
+            name,
+            email,
+            age,
+            address
+        });
+        await users.save();
+    } catch (err) {
+        console.error(err);
+    }
+
+    if(!users){
+        return res.status(404).json({message: "Unable to add user"});
+    }
+
+    return res.status(200).json({  users });
+
+}
+
 exports.getAllUsers = getAllUsers;
+exports.addUser = addUser;
