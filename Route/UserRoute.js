1fe3a6f3eb9c5777d9f24f9e9d1172e0bd0ff061
Heshani833 <heshanishehana123@gmail.com>
2025-08-11 11:42:40 +0530

    data insert backend part

--- Route/UserRoute.js
+++ Route/UserRoute.js
@@ -4,5 +4,7 @@ const User = require("../Model/UserModel");
 const UserControl = require("../Controlers/UserControl");
 
 router.get("/", UserControl.getAllUsers);
+router.post("/", UserControl.addUser);
+
 
 module.exports = router;
