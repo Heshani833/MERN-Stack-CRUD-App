393b2b06411e8c1cc10a18b2969f334e95aa2cd8
Heshani833 <heshanishehana123@gmail.com>
2025-08-14 19:48:14 +0530

    create user form

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
 
 const AddUser = () => {
+
+  const history = useNavigate();
+  const [inputs, setInputs] = useState({
+    name: "",
+    gmail: "",
+    age: "",
+    address: "",
+  });
+
   return (
     <>
       <Navbar />
       <div>AddUser</div>
+      <form>
+      <label>name</label>
+      <br/>
+      <input type="text" name="name" value={inputs.name} required></input>
+      <br/><br/>
+      <label>Gmail</label>
+      <br/>
+      <input type="email" name="gmail" value={inputs.gmail} required></input>
+      <br/><br/>
+      <label>Age</label>
+      <br/>
+      <input type="number" name="age" value={inputs.age} required></input>
+      <br/><br/>
+      <label>Address</label>
+      <br/>
+      <input type="text" name="address" value={inputs.address} required></input>
+      <br/><br/>
+      <button type="submit">Add User</button>
+      </form>
     </>
   );
 };
