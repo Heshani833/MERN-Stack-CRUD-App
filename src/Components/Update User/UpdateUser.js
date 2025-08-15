35a1ae26d3f4168ea080a252605f811c6fe13437
Heshani833 <heshanishehana123@gmail.com>
2025-08-15 10:23:32 +0530

    create UpdateUser file

---
+++ src/Components/Update User/UpdateUser.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+function UpdateUser() {
+  return (
+    <div>
+      <h1>Update User</h1>
+    </div>
+  )
+}
+
+export default UpdateUser
