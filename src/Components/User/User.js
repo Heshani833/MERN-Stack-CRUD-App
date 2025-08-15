7d20f8dfb64501bf14162596db8f65d7e7345e85
Heshani833 <heshanishehana123@gmail.com>
2025-08-15 17:17:14 +0530

    deleting data

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const User = ({ user = {} }) => {
   const { _id, name, gmail, age, address } = user;
+
+  const deleteHandler = async () => {
+    await axios
+      .delete(`http://localhost:5000/users/${_id}`)
+      .then((res) => {
+        console.log("User deleted:", res.data);
+        window.location.reload(); 
+      })
+      .catch((err) => {
+        console.error("Error deleting user:", err);
+      });
+  };
 
   return (
     <div>
@@ -13,8 +26,7 @@ const User = ({ user = {} }) => {
       <h2>Age: {age}</h2>
       <h2>Address: {address}</h2>
       <Link to={`/user-details/${_id}`}>Update</Link>
-      <button>Delete</button>
-
+      <button onClick={deleteHandler}>Delete</button>
       <br />
       <br />
       <br />
