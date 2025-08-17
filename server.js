f6076f5477bb8b465bf75af2671e3fef0c95378e
Heshani833 <heshanishehana123@gmail.com>
2025-08-17 14:47:56 +0530

    resolve errors

---
+++ server.js
@@ -0,0 +1,30 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mern_crud", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
